7a742becd216f6ed015bb8d911d81ed510afcf60
juligarc91 <julissagarcia@pursuit.org>
2024-06-06 11:13:59 -0400

    created search function

--- index.js
+++ index.js
@@ -16,6 +16,21 @@ const parse = async url => {
 }
 parse(feedURL);
 
+// Function to filter articles based on categories
+const filterArticlesByCategory = (articles, category) => {
+    return articles.filter(article => {
+        if (article.item.categories) {
+            return article.item.categories.some(cat => cat._ === category);
+        } else {
+            return false; // Or handle as needed, such as skipping these articles
+        }
+    });
+};
+
+
+// make another one maybe with https://rss.nytimes.com/services/xml/rss/nyt/Climate.xml
+// to search categories Ocean (4 articles), Shipwrecks (1 article)
+
 // creating server and requests
 let app = express();
 app.use(cors());
@@ -23,6 +38,17 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send(articles);
 })
+
+// Search endpoint
+app.get('/search', (req, res) => {
+    const category = req.query.category;
+    if (!category) {
+        return res.status(400).json({ error: "Category parameter is required." });
+    }
+
+    const filteredArticles = filterArticlesByCategory(articles, category);
+    res.json(filteredArticles);
+});
 
 const server = app.listen("3000", () =>{
     console.log("App is listening at http://localhost:3000");
