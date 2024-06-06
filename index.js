0e28ff7e6505326704169db46344a9063a1219a0
juligarc91 <julissagarcia@pursuit.org>
2024-06-06 13:31:19 -0400

    removed search by category endpoint

--- index.js
+++ index.js
@@ -2,56 +2,33 @@ import cors from "cors";
 import express from "express";
 import RSSParser from "rss-parser";
 
-// parsing feed
-const feedURL = "https://rss.nytimes.com/services/xml/rss/nyt/World.xml"
 const parser = new RSSParser();
 let articles = [];
-const parse = async url => {
-    const feed = await parser.parseURL(url);
+
+const parse = async (section) => {
+    const feedURL = `https://rss.nytimes.com/services/xml/rss/nyt/${section}.xml`;
+    const feed = await parser.parseURL(feedURL);
     console.log(feed.title);
     feed.items.forEach(item => {
-        // console.log(`${item.title}\n${item.link}\n\n`)
-        articles.push({ item })
-    })
-}
-parse(feedURL);
-
-// Function to filter articles based on categories
-const filterArticlesByCategory = (articles, category) => {
-    return articles.filter(article => {
-        if (article.item.categories) {
-            return article.item.categories.some(cat => cat._ === category);
-        } else {
-            return false; // Or handle as needed, such as skipping these articles
-        }
+        articles.push({ item });
     });
 };
 
-
-// make another one maybe with https://rss.nytimes.com/services/xml/rss/nyt/Climate.xml
-// to search categories Ocean (4 articles), Shipwrecks (1 article)
-
-// creating server and requests
-let app = express();
+const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send(articles);
-})
-
-// Search endpoint
-app.get('/search', (req, res) => {
-    const category = req.query.category;
-    if (!category) {
-        return res.status(400).json({ error: "Category parameter is required." });
+app.get('/section', async (req, res) => {
+    const section = req.query.section;
+    if (!section) {
+        return res.status(400).json({ error: "Section parameter is required." });
     }
-
-    const filteredArticles = filterArticlesByCategory(articles, category);
-    res.json(filteredArticles);
+    articles = []; // reset articles array
+    await parse(section);
+    res.send(articles);
 });
 
-const server = app.listen("3000", () =>{
+const server = app.listen(3000, () => {
     console.log("App is listening at http://localhost:3000");
-})
+});
 
 export default server;
